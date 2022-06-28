3ebaa0a787368d6a5f306b5ace8894d4c3d451ea
NaziraEU8 <nazirakrt@gmail.com>
2022-06-28 22:39:23 +0200

    initial commit

---
+++ src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.cydeo.centrilli.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/cydeo/centrilli/stepDefinitions",
+        dryRun = true,
+        tags = ""
+
+)
+
+public class CukesRunner {
+
+}
